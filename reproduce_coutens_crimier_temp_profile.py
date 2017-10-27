6a6b14a56c3b82c94a0cd192e3a96ccf3d4fa0f4
Tom Rice <t.rice90@gmail.com>
2017-10-27 14:03:52 +0200

    changed the r0 situation (for the better) and added a lot of code

--- reproduce_coutens_crimier_temp_profile.py
+++ reproduce_coutens_crimier_temp_profile.py
@@ -13,24 +13,26 @@ from matplotlib import pyplot as plt
 from scipy.integrate import trapz as integrate
 import scipy.interpolate
 
-import astroProcs
-import natconst as nc
-import transphereProcs as tP
-import plotTransphere
+from transphere_python import astroProcs
+import transphere_python.natconst as nc
+import transphere_python.transphereProcs as tP
+from transphere_python import plotTransphere
 
 ### Parameters of physical model
 
-lstar    = 3                 # Stellar luminosity in units of solar luminosities
-tstar    = 1000.             # Stellar temperature
+lstar    = 22                # Stellar luminosity in units of solar luminosities
+tstar    = 5000.             # Stellar temperature
 rstar    = lstar**0.5*(tstar/5785.0)**(-2.0)*nc.RS
-rin      = 1.0 * nc.AU       # Inner radius of shell
-rout     = 1.0e4 * nc.AU     # Outer radius of shell
-r0       = 10.0 * nc.AU      # Reference radius
+rin      = 22 * nc.AU        # Inner radius of shell
+rout     = 6100 * nc.AU      # Outer radius of shell
+# r0       = 76 * nc.AU        # Reference radius (I think this is right?) where T_dust=100K
+rinf     = 1280 * nc.AU      # Radius at which density distribution changes
 #rho0     = 9.0e-16          # Density at reference radius (gas mass). Can be used instead of Menv (need to change code further down)
-menv     = 0.5               # Envelope mass in M_sun
-plrho    = -1.5              # Powerlaw for rho
+menv     = 1.9               # Envelope mass in M_sun
+plrho_inner= -1.5              # Powerlaw for rho inside r_inf
+plrho_outer= -2                # Powerlaw for rho outside r_inf
 isrf     = 0.0               # Scaling of ISRF
-dist     = 235.0             # Distance in pc
+dist     = 120               # Distance in pc
 tbg      = -1                # Spectral shape of ISRF (Blackbody equivalent temperature). With -1 the spectrum is read from the file isrf.inp and scaled by ISRF.
 
 ### Parameters related to control of code
@@ -52,3 +54,61 @@ tP.writeopac(o, localdust, nr)      ### Write-out opacity as function of radius
 kappa = tP.findKappa(localdust, o)  ### Find Kappa at 0.55 micron
 
 r = tP.makeRadialGrid(nref, rin, rout, rref, nr)  # Make radial grid
+
+print(r)
+print(len(r))
+
+import astropy.constants as c
+import astropy.units as u
+
+# 2.89 comes from this calculation:
+# ((45*2*c.m_p + 10*4*c.m_p)/(45))/c.m_p
+number_density_to_mass_density_of_H2 = 2.89*c.m_p
+
+n_at_T_dust_100K = 2e8 * u.cm**-3
+rho_at_T_dust_100K = (n_at_T_dust_100K * number_density_to_mass_density_of_H2).to(u.g*u.cm**-3)
+
+n_at_rinf = 2.5e6 * u.cm**-3
+rho_at_rinf = (n_at_rinf * number_density_to_mass_density_of_H2).to(u.g*u.cm**-3)
+
+# print 4.0*math.pi/(3.0+plrho)*rho0*r0**(-plrho)*(rout**(3.0+plrho)-rin**(3.0+plrho))/1.989e33
+# If rho0 is given above then uncomment here.
+# rho0 = menv/(4.0*math.pi/(3.0+plrho)*r0**(-plrho) *
+#              (rout**(3.0+plrho)-rin**(3.0+plrho))/1.989e33)
+
+# n_r_nought = 
+
+rho = np.zeros_like(r)
+rho[r<=rinf] = rho_at_rinf.value * (rinf/r[r<=rinf])**(-plrho_inner)
+rho[r>=rinf] = rho_at_rinf.value * (rinf/r[r>=rinf])**(-plrho_outer)
+
+print(rho)
+
+# rho = 1e-2*rho0 * (r/r0)**(plrho)
+#tau    = integrate(rho*kappa,r)
+# print 'Tau = ',tau
+
+model = {"rstar": rstar, "mstar": 1.0, "tstar": tstar, "r": r, "rho": rho, "isrf": isrf, "tbg": tbg, 
+    "freq": o['freq'], "nriter": nriter, "convcrit": convcrit, "ncst": ncst, "ncex": ncex, 
+    "ncnr": ncnr, "itypemw": itypemw, "idump": idump}
+tP.writeTransphereInput(model)
+
+os.system('transphere')  # Change this to a popen call or something
+
+# import transphere_python.readSed
+
+# sed = transphere_python.readSed.readSed('sed.dat')
+
+
+s = tP.readSpectrum()
+a = tP.readConvhist()
+
+plt.figure(1)
+plotTransphere.plotSpectrum(s, dpc=dist, jy=1, pstyle='b-')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim((1, 3.0e3))
+plt.ylim((1.0e-9, 1.0e3))
+z = {"freq": o['freq'], "spectrum": math.pi *
+     astroProcs.bplanck(o['freq'], tstar)*rstar**2/nc.pc**2}
+plotTransphere.plotSpectrum(z, dpc=dist, jy=1, pstyle='g--')
