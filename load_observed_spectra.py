bb9e9e4362a04decfbacd3d161ebf4c2565d7f9b
Tom Rice <t.rice90@gmail.com>
2017-10-29 15:29:14 +0100

    basic file

---
+++ load_observed_spectra.py
@@ -0,0 +1,22 @@
+
+
+def retrieve_herschel_spectrum():
+
+    # We'll figure this out at some point.
+
+    pass
+
+
+def retrieve_iram_spectrum():
+
+    # also to be figured out.
+
+    pass
+
+
+def retrieve_jcmt_spectrum():
+
+    # ok.
+
+    pass
+
