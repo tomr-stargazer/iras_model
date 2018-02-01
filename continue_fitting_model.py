1c2ffe71f368f0ca6e2212812345f3c7f01cfc0d
Tom Rice <t.rice90@gmail.com>
2018-02-01 13:42:58 -0500

    update continue fitting model code

--- continue_fitting_model.py
+++ continue_fitting_model.py
@@ -2,12 +2,12 @@ import pdb
 import emcee
 from fit_model import *
 
-new_backend = emcee.backends.HDFBackend("copy2_model_chain_weekend.h5")
+new_backend = emcee.backends.HDFBackend("model_chain_wampf.h5")
 print("Initial size: {0}".format(new_backend.iteration))
 
 new_sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(data,), backend=new_backend)
 
-n_steps = 750
+n_steps = 350
 print("Ready to run {0} more steps?".format(n_steps))
 pdb.set_trace()
 new_sampler.run_mcmc(None, n_steps, progress=True)
